0e4d338fee9dbe81e4311f34c08b95911948dc59
vipin <vipinkmundra@gmail.com>
2023-01-13 11:46:52 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rw = new RWOnly();
+//        rw.name = "vipin";
+//        java: name has private access in com.driver.RWOnly
+        rw.setName("vipin");
+        String ans = rw.getName();
+        System.out.println(ans);
+
+    }
 }
